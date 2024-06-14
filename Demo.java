81e2ecbc709f4482cf60451db90e09ff8bd6f736
Yash shinde <yashshinde.tech@gmail.com>
2024-06-14 14:40:37 +0530

    servlet

--- Demo.java
+++ Demo.java
@@ -1,23 +1,24 @@
 
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class Demo
  */
 public class Demo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
     public Demo() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -26,8 +27,10 @@ public class Demo extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		 PrintWriter pw = response.getWriter();
-		 String 
+		PrintWriter pw = response.getWriter();
+		 String  s1 =request.getParameter("tT");
+		 pw.println("<h1>hello</h1>,"+s1);
+	
 	}
 
 	/**
@@ -36,6 +39,7 @@ public class Demo extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
